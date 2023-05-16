32eb45b244858cc9f5a35d32b687289e5fa9f5de
youcef <oyoucef099@gmail.com>
2023-05-16 19:00:35 +0100

    feat: add configs table and key row to txts table

--- init.sql
+++ init.sql
@@ -1,7 +1,15 @@
 CREATE TABLE txts (
   id SERIAL PRIMARY KEY,
+  key TEXT NOT NULL,
   en TEXT NOT NULL,
   fr TEXT NOT NULL
 );
 
-COPY txts(en, fr) FROM '/csv/txts.csv' DELIMITER ',' CSV HEADER;
+CREATE TABLE configs (
+  id SERIAL PRIMARY KEY,
+  key TEXT NOT NULL,
+  value TEXT NOT NULL,
+);
+
+COPY txts(key, en, fr) FROM '/csv/txts.csv' DELIMITER ',' CSV HEADER;
+COPY txts(key, value) FROM '/csv/configs.csv' DELIMITER ',' CSV HEADER;
