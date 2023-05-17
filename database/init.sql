49438aa8be72348e00be7c2cdc9c7eeca49cd2b5
youcef <oyoucef099@gmail.com>
2023-05-17 17:04:15 +0100

    refactor: create and initialize the db using docker

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE txts (
+CREATE TABLE texts (
   id SERIAL PRIMARY KEY,
   key TEXT NOT NULL,
   en TEXT NOT NULL,
@@ -11,5 +11,5 @@ CREATE TABLE configs (
   value TEXT NOT NULL
 );
 
-COPY txts(key, en, fr) FROM '/csv/txts.csv' DELIMITER ',' CSV HEADER;
+COPY texts(key, en, fr) FROM '/csv/texts.csv' DELIMITER ',' CSV HEADER;
 COPY configs(key, value) FROM '/csv/configs.csv' DELIMITER ',' CSV HEADER;
