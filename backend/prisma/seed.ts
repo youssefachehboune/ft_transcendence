48768cc4e7aae55f35d3b9a65c6563671ffc9650
Youssef Achehboune <yachehbo@gmail.com>
2023-05-23 17:45:51 +0100

    refactor: Update seed database implementation
    - Added untracked files: backend/prisma/csv/ and backend/prisma/seed.ts
    - Modified README.md in the backend directory
    - Deleted the database directory

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from '@prisma/client';
+import * as fs from 'fs';
+
+const prisma = new PrismaClient();
+
+const csvText = './prisma/csv/texts.csv';
+const csvConfig = './prisma/csv/configs.csv';
+
+async function seedData(): Promise<void> {
+    try {
+        const [textContents, configContents] = await Promise.all([
+            fs.promises.readFile(csvText, 'utf-8'),
+            fs.promises.readFile(csvConfig, 'utf-8')
+        ]);
+
+        const textlines = textContents.trim().split('\n').slice(1);
+        const configlines = configContents.trim().split('\n').slice(1);
+
+        for (const line of textlines) {
+            const [key, en, fr] = line.split(',');
+            const existingText = await prisma.texts.findFirst({ where: { key } });
+            if (!existingText) {
+                await prisma.texts.create({
+                    data: { key, en, fr }
+                });
+            }
+
+        }
+        console.log('✅  All Data from ' + csvText + ' seeded successfully');
+
+        for (const line of configlines) {
+            const [key, value] = line.split(',');
+            const existingConfig = await prisma.texts.findFirst({ where: { key } });
+            if (!existingConfig)
+            {
+                await prisma.configs.create({
+                    data: { key, value }
+                });
+            }
+        }
+        console.log('✅  All Data from ' + csvConfig + ' seeded successfully');
+    } catch (error) {
+        console.error('Error seeding data:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+seedData();
