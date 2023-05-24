0b8dce0274712a5fa168ccf89e0881e4465adfb2
Elmehdi Assamer <eassamer@e1r3p5.1337.ma>
2023-05-24 14:36:35 +0100

    feat (wip): i had some errors while pulling develop and now im trying to solve the problem

--- frontend/src/pages/components/landing-page/Github-button.tsx
+++
@@ -1,7 +0,0 @@
-import { BsFillArrowRightCircleFill } from "react-icons/bs";
-
-export default function GithubButton() {
-    return (
-        <button className='git'><h1>Github</h1><BsFillArrowRightCircleFill/></button>
-    );
-}
--- frontend/src/pages/components/landing-page/H1Text.tsx
+++
@@ -1,9 +0,0 @@
-export default function H1Text() {
-    return (
-        <div className='textHead'>
-            <h1>Lorem Ipsum</h1>
-            <div className='circleHead'></div>
-        </div>
-    );
-
-}
