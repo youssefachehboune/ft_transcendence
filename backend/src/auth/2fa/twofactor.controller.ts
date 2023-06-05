5c93a55dee5860fdd55e222a46d736b2dec168fc
Youssef Achehboune <yachehbo@gmail.com>
2023-06-05 18:19:00 +0100

    feat: Add 2FA genarate Secret and validate Token API.

--- backend/src/auth/2fa/twofactor.controller.ts
+++ backend/src/auth/2fa/twofactor.controller.ts
@@ -1,16 +1,35 @@
-import { Controller , Get, Req, UseGuards} from '@nestjs/common';
+import { Controller , Get, Req, Res, UseGuards} from '@nestjs/common';
 import { TwoFactorService } from './twofactor.service';
 import { JwtGuard } from '../guards/jwt.guard';
-import { Request } from 'express';
+import { Request , Response} from 'express';
 
 @Controller('2fa')
 export class TwoFactorController {
     constructor(private twoFactorService: TwoFactorService){}
     
+    // 2FA status endpoint (GET /2fa/status)
+    // This endpoint should return boolean value
     @Get('status')
     @UseGuards(JwtGuard)
-    async getTwoFactorStatus(@Req() req: Request)
+    async getTwoFactorStatus(@Req() req: Request): Promise<boolean>
     {
         return this.twoFactorService.getTwoFactorStatus(req);
+    }
+
+    // 2FA generate secret endpoint (GET /2fa/generate)
+    // This endpoint should return QR code image
+    @Get('generate')
+    @UseGuards(JwtGuard)
+    async generateSecret(@Req() req: Request, @Res() res: Response) {
+        return this.twoFactorService.generateSecret(req, res);
+    }
+
+    // 2FA validate token endpoint (POST /2fa/validate)
+    // This endpoint should return boolean value of token is valid or not
+    @Get('validate')
+    @UseGuards(JwtGuard)
+    async validateTwoFactor(@Req() req: Request): Promise<boolean>
+    {
+        return this.twoFactorService.validateTwoFactor(req);
     }
 }
--- backend/src/auth/2fa/twofactor.service.ts
+++ backend/src/auth/2fa/twofactor.service.ts
@@ -1,12 +1,16 @@
-import { Injectable, Req } from '@nestjs/common';
+import { Injectable, Req , Res} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { Request } from 'express';
+import { Response, Request } from 'express';
+import { authenticator } from 'otplib';
+import { toFileStream } from 'qrcode';
+
 
 const prisma = new PrismaClient();
 
 @Injectable()
 export class TwoFactorService {
-    async getTwoFactorStatus(@Req() req: Request)
+
+    async getTwoFactorStatus(@Req() req: Request): Promise<boolean>
     {
         const user = await prisma.user.findUnique({
             where: {
@@ -14,5 +18,53 @@ export class TwoFactorService {
             }
         })
         return user.twoFactorEnabled;
+    }
+
+    async pipeQrCodeStream(stream: Response, otpauthUrl: string) {
+        return toFileStream(stream, otpauthUrl);
+    }
+    async generateSecret(@Req() req: Request, @Res() res: Response) {
+        const secret = authenticator.generateSecret();
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.user['email']
+            }
+        })
+        await prisma.user.update({
+            where: {
+                email: req.user['email']
+            },
+            data: {
+                twoFactorSecret: secret
+            }
+        })
+        const otpauthUrl = authenticator.keyuri(user.email, 'transcendence', secret);
+        return this.pipeQrCodeStream(res, otpauthUrl)
+    }
+
+    async validateTwoFactor(@Req() req: Request): Promise<boolean>
+    {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: req.user['email']
+            }
+        })
+        if(user === null || user.twoFactorSecret === null) {
+            return false;
+        }
+        const token = req.body['token'];
+        const isValid = authenticator.verify({token , secret: user.twoFactorSecret });
+        if (isValid) {
+            await prisma.user.update({
+                where: {
+                    email: req.user['email']
+                },
+                data: {
+                    twoFactorEnabled: true
+                }
+            })
+            return true;
+        }
+        return false;
     }
 }
