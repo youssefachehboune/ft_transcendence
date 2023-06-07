8c360ef08ed5ed552de8b88c109f9965d4631dce
Elmehdi Assamer <eassamer@e1r3p14.1337.ma>
2023-06-07 14:49:45 +0100

    feat : change some styles in language component change animation buttons clear my code

--- frontend/src/pages/components/landing-page/Lang-div.tsx
+++ frontend/src/pages/components/landing-page/Lang-div.tsx
@@ -5,11 +5,11 @@ export default function LangDiv() {
         <div className='lang-div'>
             <button onClick={() => {document.cookie = "lang=fr";location.reload()}} className='fr-button'>
             <img src="france.png" alt="" />
-                &nbsp; French
+                Francais
             </button>
             <button onClick={() => {document.cookie = "lang=en";location.reload()}} className='en-button'>
                     <img src="english.png" alt="" />
-                    &nbsp; English
+                    English
             </button>
         </div>
     );
