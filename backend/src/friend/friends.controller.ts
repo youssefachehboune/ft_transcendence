3313c636c4311b36b594dbc21e3fa0a23987c940
Youssef Achehboune <yachehbo@gmail.com>
2023-06-09 12:07:45 +0100

    feat: Add API endpoint to get all friends of user

--- backend/src/friend/friends.controller.ts
+++ backend/src/friend/friends.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FriendsService } from './friends.service';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @ApiTags('friends')
 @Controller('friends')
 export class FriendsController {
     constructor(private readonly friendsService: FriendsService) {}
-    
+
     @Get()
-    async getFriends() {
-        return await this.friendsService.getFriends();
+    @UseGuards(JwtGuard)
+    async getFriends(@Req() req: Request) {
+        return await this.friendsService.getFriends(req);
     }
 }
--- backend/src/friend/friends.service.ts
+++ backend/src/friend/friends.service.ts
@@ -1,9 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaClient, User } from '.prisma/client';
+import { Injectable, Req} from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class FriendsService {
-    async getFriends() {
-        return 'Hello From friends API!';
+    private readonly prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = new PrismaClient();
     }
+    async getFriends(req: Request) {
+        try {
+          const user: User = await this.prisma.user.findUnique({
+            where: { email: req.user["email"] },
+          });
+      
+          const [friends1, friends2] = await Promise.all([
+            this.prisma.friendship.findMany({
+              where: {
+                user_id: user.id,
+                status: "FRIENDS"
+              }
+            }),
+            this.prisma.friendship.findMany({
+              where: {
+                friend_id: user.id,
+                status: "FRIENDS"
+              }
+            })
+          ]);
+      
+          const friendIds = [
+            ...friends1.map(element => element.friend_id),
+            ...friends2.map(element => element.user_id)
+          ];
+      
+          const profileFriends = await this.prisma.userProfile.findMany({
+            where: {
+              user_id: { in: friendIds }
+            },
+            select: {
+              user_id: true,
+              avatar: true,
+              firstName: true,
+              lastName: true,
+              username: true,
+            }
+          });
+      
+          return profileFriends;
+        } catch (error) {
+          console.error("An error occurred:", error);
+          return {
+            message: "An error occurred",
+          };
+        }
+      }
 
 }
