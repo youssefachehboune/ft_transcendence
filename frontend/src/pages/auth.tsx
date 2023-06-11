3a9bc9a4cb1a2f55013c2ccd917e28433df8b564
eassamer <asmrmehdi6@gmail.com>
2023-06-11 22:07:31 +0100

    feat : add last componenet called loading in success component and add rectangle component and test again responsive

--- frontend/src/pages/auth.tsx
+++ frontend/src/pages/auth.tsx
@@ -22,6 +22,7 @@ export default function auth() {
             } */}
             <AuthLogin />
             <Text />
+            <Rectangle />
         </div>
     );
 }
--- frontend/src/pages/components/auth/Success.tsx
+++ frontend/src/pages/components/auth/Success.tsx
@@ -1,5 +1,5 @@
 import { AiOutlineSafetyCertificate } from "react-icons/ai";
-
+import Loading from "./Loading";
 
 
 export default function Success() {
@@ -7,7 +7,7 @@ export default function Success() {
         <div className="success">
             <AiOutlineSafetyCertificate className="icon_success" color="#5ACDA4"/>
             <h1 className="h_success">Two-Factor<br/>Authentication Verfied</h1>
-            <p className="p_success">You are being redirected ...</p>
+            <p className="p_success">You are being redirected<Loading/></p>
         </div>
     );
 
