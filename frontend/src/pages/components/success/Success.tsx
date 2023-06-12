9be8aa248661c9e4132c9078659093f4503fde69
mbjaghou <mouhamedbj61@gmail.com>
2023-06-12 14:46:40 +0100

    feat: add Loading animation to  success page

--- frontend/src/pages/components/success/Success.tsx
+++ frontend/src/pages/components/success/Success.tsx
@@ -2,6 +2,7 @@ import { BsEmojiLaughing } from 'react-icons/bs'
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { getText } from "../../api/lang";
+import Loading from './Loading';
 function Success_div()
 {
 	const router = useRouter();
@@ -19,8 +20,7 @@ function Success_div()
 			<div className='flex w-full h-[250px] phone:h-[180px] Large-phone:h-[200px] flex-col items-center'>
 			<BsEmojiLaughing className='emoji_icone' color='#5ACDA4'/>
 			<h1 className='success_title'>{getText('SUCCESS')}</h1>
-			<h1 className='success_text test phone:before:right-[12%] laptop:before:right-[10%] Large-phone:before:right-[15%]'>{getText('REDIRECT')} <span className='text-[24px]'>...</span></h1>
-
+			<h1 className='success_text'>{getText('REDIRECT')}<Loading/></h1>
 			</div>
 		</div>
 	)
