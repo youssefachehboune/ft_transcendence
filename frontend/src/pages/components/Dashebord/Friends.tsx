278f03e3f796fa55aa1d44a4903313386ceaa9c0
Mohamed Bjaghou <mbjaghou@e1r3p8.1337.ma>
2023-06-19 16:58:35 +0100

    feat: add Add lastHistor feature to show friend profile

--- frontend/src/pages/components/Dashebord/Friends.tsx
+++ frontend/src/pages/components/Dashebord/Friends.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
-import { BsClock, BsPeople } from "react-icons/bs";
+import { useState, KeyboardEvent } from "react";
+import { AiFillMessage } from "react-icons/ai";
+import { BsClock, BsPatchCheckFill, BsPeople, BsPersonCheckFill } from "react-icons/bs";
 import { FaBolt, FaChartBar, FaChartPie } from "react-icons/fa";
 import { IoLocationOutline } from "react-icons/io5";
 import { VscSearch } from "react-icons/vsc";
 
-function Friends({handelsearchChanges, data}: any) { 
+function Friends({data}: any) { 
     const [visible, setvisible] = useState<boolean>(false);
+    const [searchfriend, setsearchfriend] = useState<string>("");
     const obj : any = {
         one:{
             name: "mouhamed bjaghou",
@@ -120,6 +122,14 @@ function Friends({handelsearchChanges, data}: any) {
 
     };
     const test = Object.values(obj);
+    const handelsearchChanges = (e: KeyboardEvent<HTMLInputElement>) =>
+    {
+        if (e.key === 'Enter')
+        {
+            setsearchfriend(e.currentTarget.value);
+            console.log(e.currentTarget.value)
+        }
+    }
     return (
         <div className="cont overflow-hidden flex gap-[10px]" >
             <div className="profile"></div>
@@ -136,51 +146,56 @@ function Friends({handelsearchChanges, data}: any) {
                                                 className="text-white text-[8px] font-sora font-[300] flex items-center bg-transparent  border-none outline-none pl-[20px] w-[70%]"
                                                 />
                                             </div>
-                                            <div className="w-[85%] h-[30px] self-end mt-[20px]">
+                                            <div className="w-[100%] h-[30px] mt-[20px] flex justify-center items-center">
                                                     <h1 className="font-[700] font-sora text-[11px] text-white">999 Friends</h1>
                                             </div>
 
                                     </div>
                                         <div className="w-[100%] h-[100%] gap-[10px] flex flex-col">
                                             {test.map((user: any, key) => (
-                                                <div key={key} className="min-h-[61px] flex items-center">
+                                                <button onClick={() => {setvisible(!visible)}} key={key} className="min-h-[61px] flex items-center">
                                                     <div className="w-[70%] flex items-center justify-center">
                                                         <img src={user.img} alt="" className="w-[54px] rounded-full"/>
-                                                        <div className="w-[150px] h-[100%] flex flex-col justify-center ml-[3%] mb-[5%]">
+                                                        <div className="w-[150px] h-[100%] flex flex-col justify-center items-start ml-[3%] mb-[5%]">
                                                             <h1 className="text-[13px] font-sora font-[600] text-[white]">{user.name}</h1>
                                                             <h1 className="text-[10px] font-sora font-[400] text-[#969696] ">{"@" + user.login}</h1>
                                                         </div>
                                                     </div>
-                                                    <button onClick={() => {setvisible(!visible)}} className="text-white ml-[20%] mb-[10px]">
+                                                    <button className="text-white ml-[20%] mb-[10px]">
                                                         ...
                                                     </button>
-                                                </div>
+                                                </button>
                                             ))}
                                         </div>
             </div>
             {
                 visible &&
-                    <div className="w-[60%] h-[100%] test5 rounded-[10px] xl:hidden 2xl:hidden flex flex-col relative">
-                        <img src="mbjaghou.jpeg" alt="" className="w-[131px] h-[131px] absolute top-[5%] left-[5%] rounded-full"/>
+                    <div className="w-[60%] h-[100%] test5 rounded-[10px] xl:hidden 2xl:hidden flex flex-col">
+                        <img src="mbjaghou.jpeg" alt="" className="w-[7%] rounded-full absolute top-[90px] ml-[30px]"/>
                         <div className="w-[100%] h-[10%] test5"></div>
-                        <div className="w-[100%] h-[10%]">
-                            <div className="w-[80%] h-[50%]  flex flex-col items-center justify-center">
-                                <h1 className="text-white font-[700] font-sora text-[20px]">mouhamed bjaghou</h1>
+                        <div className="w-[100%] h-[10%] flex flex-col items-end">
+                            <div className="w-[70%] h-[50%]  flex flex-col justify-center">
+                                <h1 className="text-white font-[700] font-sora text-[20px] flex items-center">mouhamed bjaghou<span className="ml-[5px]"><BsPatchCheckFill color="#2CBDE6"/></span></h1>
                                 <h1 className="text-white font-[400] font-sora text-[15px]">@mbjaghou</h1>
                             </div>
-                            <div className="w-[70%] h-[50%] flex gap-[10px] items-center justify-end">
-                                    <button className="w-[88px] h-[24px] test5 rounded-[4px] text-white">test</button>
-                                    <button className="w-[88px] h-[24px] bg-white rounded-[4px] ">test</button>
+                            <div className="w-[70%] h-[50%] flex gap-[10px] items-center justify-center">
+                                    <button className="w-[88px] h-[24px] test5 rounded-[4px] hover:bg-[#00DAEA]"><h1 className="text-white font-sora text-[11px] font-400 flex items-center ml-[10px]"><span className="mr-[5px]"><BsPersonCheckFill/></span>Friends</h1></button>
+                                    <button className="w-[88px] h-[24px] test5 text-white rounded-[4px] font-sora text-[11px] font-400 hover:bg-[#00DAEA]"><h1 className="text-white font-sora text-[11px] font-400 flex items-center ml-[10px]"><span className="mr-[5px]"><AiFillMessage/></span>message</h1></button>
+                                    <select id="block" className="w-[30px]">
+                                        <option value=""></option>
+                                        <option value="">block</option>
+                                    </select>
                             </div>
                         </div>
                         <div className="w-[100%] h-[15%] ">
                             <h1 className="font-sora font-[700] text-[15px] p-[10px] text-white">Bio</h1>
                             <p className="font-sora font-[300] text-[13px] p-[10px] text-white">Wanderlust soul exploring the world one adventure at a time. ✈️🗺️ Sharing my passions: travel, photography, and embracing every moment. 📸✨ Join med inspiration. ✌️❤️</p>
                         </div>
-                        <div className="w-[95%] h-[30%] flex">
-                                <div className="w-[50%] h-[100%] flex flex-col justify-center items-end">
-                                        <div className="w-[95%] h-[50%] flex flex-col justify-center gap-[20px] mb-[110px]">
-                                                <h1 className="text-[white] text-[15px] font-sora font-[700] pl-[10px]">info</h1>
+                        <div className="w-[95%] h-[25%] flex relative">
+                                <div className="w-[2px] h-[180px] top-[5%] left-[50%] test5 absolute"></div>
+                                <div className="w-[50%] h-[100%]">
+                                        <div className="w-[100%] h-[50%] flex flex-col justify-around ml-[10px]">
+                                                <h1 className="text-[white] text-[15px] font-sora font-[700] pl-[10px] mt-[-10px]">info</h1>
                                                 <h1 className="text-[12px] text-[white] font-sora font-[400] flex items-center ml-[5px]"><span className="mr-[5px]"><IoLocationOutline  className="w-[17px] h-[17px]"/></span>{data.info?.location}</h1>
                                                 <h1 className="text-[12px] text-[white] font-sora font-[400] flex items-center ml-[5px]"><span className="mr-[5px]"><BsPeople className="w-[17px] h-[17px]"/></span>{data.info?.count_friends} Friends</h1>
                                                 <h1 className="text-[12px] text-[white] font-sora font-[400] flex items-center ml-[5px]"><span className="mr-[5px]"><BsClock className="w-[17px] h-[17px]"/></span>{data.info?.member_since}</h1>
@@ -188,7 +203,7 @@ function Friends({handelsearchChanges, data}: any) {
                                         </div>
                                 </div>
                                 <div className="w-[50%] h-[100%]">
-                                                <div className="w-[100%] h-[100%] flex flex-col items-center justify-center gap-[10px] ml-[20px]">
+                                                <div className="w-[100%] h-[100%] flex flex-col items-center  ml-[20px]">
                                                         <h1 className="text-[white] text-[15px] font-sora font-[700] pl-[10px] self-start">Statistics</h1>
                                                             <div className="w-[100%] h-[56px] flex items-center gap-4">
                                                                 <div className="test11 w-[32px] h-[32px] bg-orange rounded-full flex flex-col justify-center items-center"><img src="hhhhhhhh.svg" alt="" /></div>
@@ -208,6 +223,31 @@ function Friends({handelsearchChanges, data}: any) {
                                                         </div>
                                                 </div>
                                 </div>
+                        </div>
+                        <h1 className="text-[white] font-sora text-[15px] p-5">Last History</h1>
+                        <div className="w-[90%] h-[30%] test5 self-center rounded-[20px] flex flex-col justify-center gap-[10px] overflow-y-auto">
+                        {test.slice(0, 4).map((user: any, key) => (
+                            <div key={key} className="w-[100%] min-h-[65px] text-white flex overflow-hidden">
+                                <div className="w-[33.5%] flex items-center justify-end">
+                                    <img src={user.img} alt="" className="w-[54px] rounded-full"/>
+                                    <div className="w-[100px] h-[100%] flex flex-col justify-center ml-[3%] mb-[5%]">
+                                        <h1 className="text-[7px] font-sora font-[600] text-[white] ">{user.name}</h1>
+                                        <h1 className="text-[7px] font-sora font-[400] text-[#969696] ">{"@" + user.login}</h1>
+                                    </div>
+                                </div>
+                                <div className="w-[33.5%] flex flex-col items-center justify-center">
+                                    <h1 className="text-[22px] font-sora font-[400] text-[white]">7 : 5</h1>
+                                    <h1 className="text-[9px] font-sora font-[400] text-[#B3B3B3]">1 Hours ago</h1>
+                                </div>
+                                <div className="w-[33.5%] flex items-center justify-start">
+                                    <img src={user.img} alt="" className="w-[54px] rounded-full"/>
+                                    <div className="w-[100px] h-[100%] flex flex-col justify-center ml-[3%] mb-[5%]">
+                                        <h1 className="text-[7px] font-sora font-[600] text-[white] ">{user.name}</h1>
+                                        <h1 className="text-[7px] font-sora font-[400] text-[#969696] ">{"@" + user.login}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
                         </div>
                     </div>
 
