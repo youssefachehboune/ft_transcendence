568b85f32687744ce3e27af67721c222c219dfc7
Youssef Achehboune <yachehbo@e3r2p3.1337.ma>
2023-07-08 16:47:05 +0100

    feat: Implement getters, update channels table, and add channelMembers table to Prisma schema

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Req, UseGuards, Param } from '@nestjs/common';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { ChannelService } from './channel.service';
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { Request } from 'express';
@@ -10,8 +10,34 @@ export class ChannelController {
     constructor(private readonly channelService: ChannelService) {}
 
     @UseGuards(JwtGuard)
-    @Get()
-    async getChannel(@Req() req: Request) {
-        return await this.channelService.getChannel(req);
+    @Get('my_channels')
+    async getMyChannels(@Req() req: Request) {
+        return await this.channelService.getMyChannels(req);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get('public_channels')
+    async getPublicChannels(@Req() req: Request) {
+        return await this.channelService.getPublicChannels(req);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get(':channel_id')
+    @ApiParam({ name: 'channel_id', type: Number })
+    async getChannel(@Req() req: Request, @Param('channel_id') channel_id: number) {
+        return await this.channelService.getChannel(req, channel_id);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get(':channel_id/members')
+    @ApiParam({ name: 'channel_id', type: Number })
+    async getChannelMembers(@Req() req: Request, @Param('channel_id') channel_id: number) {
+        return await this.channelService.getChannelMembers(req, channel_id);
+    }
+
+    @UseGuards(JwtGuard)
+    @Get('invitations')
+    async getInvitations(@Req() req: Request) {
+        return await this.channelService.getInvitations(req);
     }
 }
--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -1,9 +1,189 @@
 import { Injectable, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { PrismaClient } from '.prisma/client';
+import { User } from '@prisma/client';
 
+const prisma = new PrismaClient();
 @Injectable()
 export class ChannelService {
-    getChannel(@Req() req: Request) {
-        return { message: "getChannel" };
+
+    async searchUserById(id: number) {
+        return await prisma.user.findUnique({
+            where: {
+                id: id,
+            },
+        });
     }
+
+
+    async getMyChannels(@Req() req: Request) {
+        const user = await this.searchUserById(req.user['id']);
+        if (!user) {
+            return 'User not found';
+        }
+        const myChannels = await prisma.channelMembers.findMany({
+            where: {
+                user_id: user.id,
+            },
+            include: {
+                Channel: true,
+            },
+        });
+        return myChannels.map((channel) => {
+            return {
+                id: channel.Channel.id,
+                name: channel.Channel.name,
+                Type: channel.Channel.type,
+                createdAt: channel.Channel.createdAt,
+                updatedAt: channel.Channel.updatedAt,
+            };
+        }
+        );
+    }
+
+    async getPublicChannels(@Req() req: Request) {
+
+        const publicChannels = await prisma.channel.findMany({
+            where: {
+                type: 'PUBLIC',
+                NOT: {
+                    ChannelMembers: {
+                        some: {
+                            user_id: req.user['id'],
+                        },
+                    },
+                },
+            },
+        });
+
+        return publicChannels.map((channel) => {
+            return {
+                id: channel.id,
+                name: channel.name,
+                Type: channel.type,
+                createdAt: channel.createdAt,
+                updatedAt: channel.updatedAt,
+            };
+        }
+        );
+    }
+
+    async getChannel(@Req() req: Request, channel_id: number) {
+        channel_id = Number(channel_id);
+        const channel = await prisma.channel.findUnique({
+            where: {
+                id: channel_id,
+            },
+            include: {
+                ChannelMembers: true,
+            },
+        });
+        if (!channel) {
+            return 'Channel not found';
+        }
+        if (channel.type === 'PUBLIC') {
+            return {
+                id: channel.id,
+                name: channel.name,
+                Type: channel.type,
+                createdAt: channel.createdAt,
+                updatedAt: channel.updatedAt,
+            };
+        }
+        else {
+            const user = await this.searchUserById(req.user['id']);
+            if (!user) {
+                return 'User not found';
+            }
+            const channelMember = await prisma.channelMembers.findFirst({
+                where: {
+                    channel_id: channel_id,
+                    user_id: user.id,
+                },
+            });
+            if (!channelMember) {
+                return 'User is not a member of the channel';
+            }
+            return {
+                id: channel.id,
+                name: channel.name,
+                Type: channel.type,
+                createdAt: channel.createdAt,
+                updatedAt: channel.updatedAt,
+            };
+        }
+    }
+
+
+    async getChannelMembers(@Req() req: Request, channel_id: number) {
+        channel_id = Number(channel_id);
+        const channel = await prisma.channel.findUnique({
+            where: {
+                id: channel_id,
+            },
+            include: {
+                ChannelMembers: true,
+            },
+        });
+        if (!channel) {
+            return 'Channel not found';
+        }
+        const user = await this.searchUserById(req.user['id']);
+        if (!user) {
+            return 'User not found';
+        }
+        const channelMember = await prisma.channelMembers.findFirst({
+            where: {
+                channel_id: channel_id,
+                user_id: user.id,
+            },
+        });
+        if (!channelMember) {
+            return 'User is not a member of the channel';
+        }
+        const channelMembers = await prisma.channelMembers.findMany({
+            where: {
+                channel_id: channel_id,
+            },
+            include: {
+                User: true,
+            },
+        });
+
+        return channelMembers.map((channelMember) => {
+            return {
+                id: channelMember.User.id,
+                email: channelMember.User.email,
+                type: channelMember.MemberType
+            };
+        }
+        );
+    }
+
+    async getInvitations(@Req() req: Request) {
+        const user = await this.searchUserById(req.user['id']);
+        if (!user) {
+            return 'User not found';
+        }
+        const invitations = await prisma.channelMembers.findMany({
+            where: {
+                user_id: user.id,
+                MemberType: 'INVITED',
+            },
+            include: {
+                Channel: true,
+            },
+        });
+        return invitations.map((invitation) => {
+            return {
+                id: invitation.Channel.id,
+                name: invitation.Channel.name,
+                Type: invitation.Channel.type,
+                createdAt: invitation.Channel.createdAt,
+                updatedAt: invitation.Channel.updatedAt,
+            };
+        }
+        );
+    }
+
 }
