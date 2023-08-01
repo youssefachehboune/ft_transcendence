028467deb15b40697d187237a0a87ebf452528bc
Youssef Achehboune <yachehbo@e3r4p12.1337.ma>
2023-08-01 18:20:43 +0100

    feat: implement a user status gateway

---
+++ backend/src/user/user-status.gateway.ts
@@ -0,0 +1,114 @@
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage } from "@nestjs/websockets";
+import { Socket, Server } from "socket.io";
+import { AuthService } from "../auth/auth.service";
+import { FriendsService } from "../friend/friends.service";
+import { v4 as uuidv4 } from 'uuid';
+
+interface User {
+    userId: number;
+    socketId: string;
+}
+
+interface Player {
+    userId: number;
+    socketId: string;
+    score: number;
+}
+
+interface GameData{
+    gameId: string;
+    player1: Player;
+    player2: Player;
+}
+
+@WebSocketGateway({ cors: { origin: '*' }, namespace: "online" })
+export class UserStatusGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    constructor(
+        private authService: AuthService,
+        private friendService: FriendsService
+    ) { }
+
+    private users: User[] = [];
+
+
+    private async getUserId(socket: Socket) {
+        const cookieHeaderValue = socket.request.headers.cookie?.split(";");
+        const jwt = cookieHeaderValue?.at(0)?.split("=")[1];
+        if (jwt) {
+            const payload = await this.authService.getUserFromToken(jwt);
+            return payload["sub"];
+        }
+        return null;
+    }
+
+    private addUser(userId: number, socketId: string) {
+        if (!this.users.some((user) => user.userId === userId)) {
+            this.users.push({ userId, socketId });
+            console.log(this.users);
+        }
+    }
+
+    private removeUser(socketId: string) {
+        this.users = this.users.filter((user) => user.socketId !== socketId);
+        console.log(this.users);
+    }
+
+    private async getFriendsOnline(userId: number) {
+        const friends = await this.friendService.getFriendsFromUser(userId);
+        if (friends?.length === 0) return [];
+        return this.users.filter((user) => friends?.includes(user.userId));
+    }
+
+
+    @WebSocketServer() server: Server;
+    async handleConnection(socket: Socket) {
+        const userId = await this.getUserId(socket);
+        console.log("new One connected", userId);
+        if (userId) {
+            this.addUser(userId, socket.id);
+            const onlineFriends = await this.getFriendsOnline(userId);
+            this.server.to(socket.id).emit("onlineFriends", onlineFriends.map((friend) => friend.userId));
+            onlineFriends.forEach((Friend) => { this.server.to(Friend.socketId).emit("onlineOne", { userId, status: true, }); });
+        }
+    }
+
+    async handleDisconnect(socket: Socket) {
+        const user = this.users.find((user) => user.socketId === socket.id);
+        console.log("One disconnected", user);
+        if (user) {
+            this.removeUser(socket.id);
+            const onlineFriends = await this.getFriendsOnline(user.userId);
+            onlineFriends.forEach((Friend) => {
+                this.server.to(Friend.socketId).emit("onlineOne", {
+                    userId: user.userId,
+                    status: false,
+                });
+            });
+        }
+    }
+
+    @SubscribeMessage("play")
+    async handlePlay(socket: Socket, data: any) {
+        const sender = this.users.find((user) => user.userId === data.sender);
+        const receiver = this.users.find((user) => user.userId === data.receiver);
+        if (receiver && sender) {
+            this.server.to(receiver.socketId).emit("invitation", data);
+        }
+    }
+
+    @SubscribeMessage("accept")
+  async handleAccept(socket: Socket, data: any) {
+    const gameId = uuidv4();
+    const sender = this.users.find((user) => user.userId === data.sender);
+    const receiver = this.users.find((user) => user.userId === data.receiver);
+    if (receiver && sender) {
+      const player1: Player = { ...sender, score: 0};
+      const player2: Player = { ...receiver, score: 0};
+        const gameData: GameData = { gameId, player1, player2 };
+      this.server.to(sender.socketId).emit("start",  gameData);
+      this.server.to(receiver.socketId).emit("start", gameData);
+    }
+  }
+
+
+}
